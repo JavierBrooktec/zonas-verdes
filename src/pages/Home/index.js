6e35f96f93b0a305a8deb8da4cf5c80c416de577
Javier Pérez Martín <jperez@brooktec.com>
2020-11-05 23:11:19 +0100

    create global state and global setter

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,112 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import { jsonParserFixed } from '../../helpers/fixes';
+import React, { useEffect, useState, useContext } from 'react';
+import SpacingGrid from '../../components/SpacingGrid';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { ThemeContext } from "../../App";
 
 import {
-  graph,
   Namespace,
-  Fetcher,
-  parse,
   sym,
-  //   serialize,
-  //   jsonParser,
 } from 'rdflib';
-import { parse as htmlParser } from 'node-html-parser';
 
 export default function Home() {
+  const [store, setMystore] = useContext(ThemeContext);
   // creamos una store
-  const [store, setstore] = useState(graph());
-
-  const me = sym('https://example.com/alice/card#me');
-  const alice = sym('https://example.com/alice/card#alice');
+  const [loading, setLoading] = useState(true);
+  const [allDistrictsInfo, setsAllDistrictsInfo] = useState([]);
 
   // crear nameSpaces
-  const VCARD = new Namespace('http://www.w3.org/2006/vcard/ns#');
-  const FOAF = new Namespace('http://xmlns.com/foaf/0.1/');
+  const ONT = new Namespace('http://api.zonasVerdesMadrid.com/ontologies/');
 
   // solo  queremos que se añadan la primera vez
   useEffect(() => {
-    store.add(me, VCARD('name'), 'Javi');
-    store.add(me, VCARD('name'), alice);
-    store.add(alice, VCARD('hasNickname'), 'Alicia777');
-  }, []);
+        const allDistricts = store
+          .match(null, ONT('districts#hasName'), null)
+          .map(({ subject, predicate, object }, i) => ({ subject, object }));
+        const newAllDistrictsInfo = allDistricts.map(({ subject, object }) => {
+          const totalSpecies = store.any(
+            sym(subject.value),
+            ONT('districts#hasTotal'),
+            null
+          );
+          return {
+            districtName: unescape(object.value),
+            totalSpecies: totalSpecies?.value,
+          };
+        });
+        setsAllDistrictsInfo(newAllDistrictsInfo);
+  }, [store]);
 
-  // añadir tripletas
 
-  // retornar tripletas
-  let nameMatch = store.match(me, VCARD('name'), null);
-  console.log(`nameMatch: ${nameMatch}`);
 
-  // retornar primer match
-  let name = store.any(me, VCARD('name'));
-  console.log(`name: ${name}`);
-
-  // añadir al store tripletas desde texto (no añade duplicados)
-  let text = '<#this>  a  <#Example> .';
-  let doc = sym('https://example.com/alice/card');
-  parse(text, store, doc.uri); // pass base URI
-
-  // macheme tripletas donde alice sea sujeto o objeto.
-  let aliceMatchers = [
-    ...store.match(alice, null, null, null).map((st) => st),
-    ...store.match(null, null, alice, null).map((st) => st),
-  ];
-
-  console.log('ALICE MATCHERS');
-  aliceMatchers.forEach(({ subject, predicate, object }) =>
-    console.log(`${subject.value} ${predicate.value} ${object.value}`)
-  );
-
-  // vamos a buscar información sobre el pino en dbpedia
-
-  // no la tenemos
-  const [loading, setloading] = useState(true);
-
-  const fetcher = new Fetcher(store);
-  const pinus = sym('http://dbpedia.org/page/Pine');
-  const profile = pinus.doc();
-
-  // lamada asinncorona que se llama solo al crear el componente
   useEffect(() => {
-    fetcher.load(profile).then(
-      (response) => {
-        const root = htmlParser(response.responseText);
-        // buscamos los links dentro de los metadatos.
-        const matched = root
-          .querySelectorAll('head link')
-          .filter((el) => el.getAttribute('type') === 'application/json');
+    setLoading(false);
+  }, [allDistrictsInfo]);
 
-        const jsonLink = matched[0].getAttribute('href');
 
-        // request del archivo
-        fetch(jsonLink)
-          .then((response) => response.json())
-          .then((data) => {
-            // jsonParser(data, 'http://dbpedia.org/data', store);
 
-            // metemos el archivo en nuestra store
-            jsonParserFixed(data, 'http://dbpedia.org/data', store);
-
-            // cuando lo hemos recibido cambiamos el estado a notLoading
-            setloading(false);
-          }); // pass base URI);
-      },
-      (err) => {}
-    );
-  }, []);
-
-  // machear la store entera y pintarla
-  const myStore = store
-    .match(null, null, null, null)
-    .map(({ subject, predicate, object }, i) => (
-      <p key={i}>
-        {subject.value} {predicate.value} {object.value}
-      </p>
-    ));
 
   return (
     <div>
-      <h1>Hellow from home!</h1>
-      {loading ? <p>loading</p> : myStore}
+      <h1>Zonas verdes de Madrid</h1>
+      {loading ? (
+        <CircularProgress color='inherit' />
+      ) : (
+        <SpacingGrid data={allDistrictsInfo}></SpacingGrid>
+      )}
     </div>
   );
 }
